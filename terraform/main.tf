0425c1c2011a3d2f1cd3076d323550ae12214090
alexpark-101 <alex.park@101.inc>
2024-01-20 16:07:45 -0800

    add lb

--- terraform/main.tf
+++ terraform/main.tf
@@ -212,6 +212,48 @@ resource "helm_release" "lb" {
   }
 }
 
+# resource "kubernetes_ingress_v1" "ingress" {
+#   wait_for_load_balancer = true
+#   metadata {
+#     name = "ingress-service"
+#     annotations = {
+#       "kubernetes.io/ingress.class" = "alb"
+#     }
+#   }
+
+#   spec {
+#     rule {
+#       http {
+#         path {
+#           path = "/*"
+#           backend {
+#             service {
+#               name = "my-superset"
+#               port {
+#                 number = 8088
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+
+#     # tls {
+#     #   secret_name = "tls-secret"
+#     # }
+#   }
+# }
+
+# # Display load balancer hostname (typically present in AWS)
+# output "load_balancer_hostname" {
+#   value = kubernetes_ingress_v1.ingress.status.0.load_balancer.0.ingress.0.hostname
+# }
+
+# # Display load balancer IP (typically present in GCP, or using Nginx ingress controller)
+# output "load_balancer_ip" {
+#   value = kubernetes_ingress_v1.ingress.status.0.load_balancer.0.ingress.0.ip
+# }
+
 ################
 #   Superset   #
 ################
@@ -243,3 +285,4 @@ resource "helm_release" "superset" {
 }
 
 #TODO extract variables
+#TODO output service name
